f9983d4ceccc4a6b36926e3ab70788a7f831bcb6
KGESH <baram789456@gmail.com>
2021-06-29 17:04:58 +0900

    publish github page

--- dist/download.js
+++ dist/download.js
@@ -1,8 +1,13 @@
 $(function(){
     $("#download").on("click", function(){
-		// 캡쳐 라이브러리를 통해서 canvas 오브젝트를 받고 이미지 파일로 리턴한다.
-    html2canvas(document.querySelector("#preview_container")).then(canvas => {
-				saveAs(canvas.toDataURL('image/png'),"capture-test.png");
+        // 캡쳐 라이브러리를 통해서 canvas 오브젝트를 받고 이미지 파일로 리턴한다.
+        html2canvas(document.querySelector("#preview_container"), {
+            allowTaint: true,
+            foreignObjectRendering: true
+        })
+        .then(canvas => {
+            console.log(canvas);
+                saveAs(canvas.toDataURL('image/png'), "capture-test.png");
 			});
     });
     function saveAs(uri, filename) { 
